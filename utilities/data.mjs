4e974676451a1dde8fa89c6338e5385adb819d7b
jfrisina <jaki.frisina@gmail.com>
2024-07-24 13:39:45 -0700

    add initial data

--- utilities/data.mjs
+++ utilities/data.mjs
@@ -1,7 +1,14 @@
 const data = [
-	{name:'Aurora', position: 'Reference', date: },
-	{},
-	{}
+	{name:'Aurora', position: 'Reference', date: '2024-08-01', starttime: '0900', endtime: '1200'},
+	{name:'Nova', position: 'Reference', date: '2024-08-02', starttime: '0900', endtime: '1200'},
+	{name:'Juniper', position: 'Reference', date: '2024-08-01', starttime: '1200', endtime: '1700'},
+	{name:'Esme', position: 'Reference', date: '2024-08-02', starttime: '1200', endtime: '1700'},
+	{name:'Willow', position: 'Circulation', date: '2024-08-01', starttime: '0900', endtime: '1200'},
+	{name:'Andre', position: 'Circulation', date: '2024-08-02', starttime: '0900', endtime: '1200'},
+	{name:'Marcus', position: 'Reference', date: '2024-08-01', starttime: '0900', endtime: '1200'},
+	{name:'Jeff', position: 'Children', date: '2024-08-02', starttime: '0900', endtime: '1200'},
+	{name:'Alex', position: 'Teen', date: '2024-08-09', starttime: '1200', endtime: '1700'},
+	{name:'Seraphina', position: 'Children', date: '2024-08-01', starttime: '1200', endtime: '1700'}
 ];
 
 
